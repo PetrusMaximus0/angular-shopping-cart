fb7169b05b781d6e31002ce7b99aff6b509daa58
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2025-05-11 21:42:37 +0100

    feat: Add a header component.

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {NavComponent} from './nav/nav.component';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    NavComponent,
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
