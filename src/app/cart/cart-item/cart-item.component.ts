03f7f5b8b952c6840d6219689ef361983fb3bf97
Pedro Silva <pedro.di.silva@ctw.bmwgroup.com>
2025-05-29 11:55:53 +0100

    feat: Add (Finalize) cart item component.

--- src/app/cart/cart-item/cart-item.component.ts
+++ src/app/cart/cart-item/cart-item.component.ts
@@ -1,29 +1,28 @@
 import {Component, input} from '@angular/core';
 import {ICartItem} from '../../shared/interfaces/ICartItem';
 import {RouterLink, RouterLinkActive} from '@angular/router';
-import {FormSubmittedEvent} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 import {CartService} from '../../shared/services/cart/cart.service';
 
 @Component({
   selector: 'app-cart-item',
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    ReactiveFormsModule
   ],
   templateUrl: './cart-item.component.html',
   styleUrl: './cart-item.component.scss'
 })
-export class CartItemComponent {
-  constructor(private cartService: CartService){
 
-  }
+export class CartItemComponent {
+  constructor(private cartService: CartService){}
 
   item = input.required<ICartItem>();
 
   protected handleNumChange($event: Event){
-    const newAmount = $event.target;
-    console.log($event);
-    this.cartService.setItemNumberOfUnits(this.item().id, 1);
+    const newAmount = parseInt(($event.target as HTMLInputElement).value);
+    this.cartService.setItemNumberOfUnits(this.item().id, isNaN(newAmount) ? 1 : newAmount);
   }
 
   protected removeItem(){
@@ -31,6 +30,10 @@ export class CartItemComponent {
   }
 
   protected incrementNum(){
-    this.cartService.
+    this.cartService.addItemToCart(this.item(), 1);
+  }
+
+  protected decrementNum(){
+    this.cartService.addItemToCart(this.item(), -1);
   }
 }
